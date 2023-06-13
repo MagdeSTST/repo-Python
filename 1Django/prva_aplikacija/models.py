57804a4e458db6414861504c16511307c1eff5b8
DESKTOP-9B4UV8M\DELL <magdestst@gmail.com>
2023-06-13 19:33:45 +0200

    Nova tabela lice

--- 1Django/prva_aplikacija/models.py
+++ 1Django/prva_aplikacija/models.py
@@ -1,3 +1,14 @@
+from typing import Any
 from django.db import models
 
 # Create your models here.
+
+#Tabela Lice(ime,prezime,email,godini,matichen broj,grad)
+
+class Lice(models.Model):
+    ime = models.CharField(max_length=50)
+    prezime = models.CharField(max_length=100)
+    email = models.EmailField
+    godini = models.IntegerField
+    matichen_broj = models.CharField(max_length=13)
+    grad = models.CharField(max_length=50)
