4d3d74ae9948319052fae4a825bbd1d52f36bc50
DESKTOP-9B4UV8M\DELL <magdestst@gmail.com>
2023-06-13 21:04:09 +0200

    Dodavanje na tabelite Klient i Naracki

--- 1Django/prva_aplikacija/admin.py
+++ 1Django/prva_aplikacija/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Lice
+from .models import Post
+from .models import Klient
+from .models import Naracka
+
+admin.site.register(Lice)
+admin.site.register(Post)
+admin.site.register(Klient)
+admin.site.register(Naracka)
+
 # Register your models here.
--- 1Django/prva_aplikacija/models.py
+++ 1Django/prva_aplikacija/models.py
@@ -1,5 +1,6 @@
 from typing import Any
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -8,7 +9,33 @@ from django.db import models
 class Lice(models.Model):
     ime = models.CharField(max_length=50)
     prezime = models.CharField(max_length=100)
-    email = models.EmailField
-    godini = models.IntegerField
-    matichen_broj = models.CharField(max_length=13)
+    ##email = models.EmailField()
+    email = models.CharField(max_length=50, blank = True)
+    godini = models.IntegerField(default=0)
+    matichen_broj = models.CharField(max_length=13, unique= True)
     grad = models.CharField(max_length=50)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100,blank=True)
+    text = models.TextField()
+    created_ad = models.DateTimeField(auto_now_add=True)
+    update_ad = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Klient(models.Model):
+    ime = models.CharField(max_length=50)
+    prezime = models.CharField(max_length=100)
+    email = models.EmailField()
+    telefonski_broj = models.CharField(max_length=30)
+    adresa_na_ziveenje = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.ime
+
+class Naracka(models.Model):
+    suma_na_narachka = models.IntegerField()
+    datum_na_narachka = models.DateTimeField(auto_now_add=True)
+    plateno = models.BooleanField()
+    klient = models.ForeignKey(Klient, on_delete=models.CASCADE)
